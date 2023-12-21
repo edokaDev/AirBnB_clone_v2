5184a423d901623c2ec0adc2caef9f739eb6668e
edoka <peter.edoka.242464@unn.edu.ng>
2023-12-21 04:20:50 +0100

    dbstorage place and update on user

--- models/user.py
+++ models/user.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.orm import Relationship
+from datetime import datetime
 
 
 class User(BaseModel, Base):
@@ -11,3 +13,7 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    places = Relationship('Place', backref='users', cascade='all, delete-orphan')
+
