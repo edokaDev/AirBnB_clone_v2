07e46d810353ce9571f596bd4d7c78e681b22914
edoka <peter.edoka.242464@unn.edu.ng>
2023-12-21 04:39:14 +0100

    added created and updated on models

--- models/city.py
+++ models/city.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy import Column, ForeignKey, String, DateTime
 from sqlalchemy.orm import Relationship
+from datetime import datetime
 
 
 class City(BaseModel, Base):
@@ -13,3 +14,5 @@ class City(BaseModel, Base):
                       nullable=False)
     name = Column(String(128), nullable=False)
     places = Relationship('Place', backref='cities', cascade='all, delete-orphan')
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
--- models/review.py
+++ models/review.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float, DateTime
+from sqlalchemy import Column, String, ForeignKey, DateTime
 from datetime import datetime
 
 
@@ -11,3 +11,5 @@ class Review(BaseModel, Base):
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
