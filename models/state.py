84dec74929c0f8c293f3e78148c20f41861c6764
edoka <peter.edoka.242464@unn.edu.ng>
2023-12-21 03:58:34 +0100

    updated models to use sqlalchemy

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.orm import Relationship
+from datetime import datetime
+from .city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    id = Column(String(60), primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = Relationship('City', backref='states', cascade='all, delete-orphan')
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+
+    @property
+    def cities(self):
+        """Returns a list of cities that have the same state_id as State.id."""
+        from models import storage
+        all_cities_dict = storage.all(City)
+        cities_list = []
+        for city in all_cities_dict.keys():
+            if State.id in city:
+                cities_list.append(all_cities_dict[city])
+        return cities_list
