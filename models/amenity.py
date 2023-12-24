ea7f9f94f7139ed3c4ccc6b5e8c4fa0bbc0cae57
edoka <peter.edoka.242464@unn.edu.ng>
2023-12-24 02:32:52 +0100

    reset to pre amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, DateTime, Table, ForeignKey
-from datetime import datetime
-from sqlalchemy.orm import relationship
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    """Amenity class."""
-
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    created_at = Column(DateTime, default=datetime.now)
-    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-    place_amenities = Table('place_amenity', Base.metadata,
-                            Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False),
-                            Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False)
-                            )
-
-    places = relationship('Place', secondary=place_amenities, back_populates='amenities')
-
+class Amenity(BaseModel):
+    name = ""
