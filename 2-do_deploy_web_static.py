4902906cfc6cde95e5e1863e8360b1613660f2a9
edoka <peter.edoka.242464@unn.edu.ng>
2024-01-16 07:29:42 +0100

    deploy archive

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""A script that distributes and archive to my web servers."""
+from fabric.api import run, env, put
+import os
+
+env.hosts = ['54.237.93.197', '54.145.238.213']
+env.user = 'ubuntu'
+
+
+def do_deploy(archive_path):
+    """Deploy archive to web server."""
+    path = archive_path.split('/')[-1]
+    line = path.split('.')[0]
+    if not os.path.isfile(archive_path):
+        return False
+    if put(archive_path, '/tmp/{}'.format(path)).failed:
+        return False
+    if run('sudo rm -rf /data/webstatic/releases/{}'.format(line)).failed:
+        return False
+    if run('sudo mkdir -p /data/web_Static/releases/{}/'.format(line)).failed:
+        return False
+    if run('sudo tar -xzf /tmp/{} -C /data/web_Static/releases/{}/'
+           .format(path, line)).failed:
+        return False
+    if run('sudo rm /tmp/{}'.format(path)).failed:
+        return False
+    if run('sudo mv /data/web_static/releases/{}/web_static/* '
+           '/data/web_static/releases/{}/'.format(line, line)).failed:
+        return False
+    if run('sudo rm -rf /data/web_static/releases/{}/web_static'
+           .format(line)).failed:
+        return False
+    if run('sudo rm -rf` /data/web_Static/current').failed:
+        return False
+    if run('sudo ln -s /data/web_static/release/{}/ /data/web_Static/current'
+           .format(line)).failed:
+        return False
+    run('sudo systemctl restart nginx')
+    return True
