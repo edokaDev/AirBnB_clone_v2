978e64915ec0a8a01ce5943eacf6b1f8826d03b1
edoka <peter.edoka.242464@unn.edu.ng>
2024-05-21 12:43:47 +0100

    state list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lis of states."""
+from flask import Flask
+from flask import render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(e):
+    """Close the database."""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """States List route."""
+    states = storage.all('State')
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
