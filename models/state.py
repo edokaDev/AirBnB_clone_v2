6aa6468d73874c3d31b9dd2bdae25467bb00e9c7
edoka <peter.edoka.242464@unn.edu.ng>
2024-05-22 23:24:41 +0100

    fix state and cities relationship

--- models/state.py
+++ models/state.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, String, DateTime
 from sqlalchemy.orm import Relationship
 from datetime import datetime
 from .city import City
+import os
 
 
 class State(BaseModel, Base):
@@ -13,19 +14,21 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     id = Column(String(60), primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = Relationship('City',
-                          backref='states',
-                          cascade='all, delete-orphan')
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
 
-    @property
-    def cities(self):
-        """Returns a list of cities that have the same state_id as State.id."""
-        from models import storage
-        all_cities_dict = storage.all(City)
-        cities_list = []
-        for city in all_cities_dict.keys():
-            if all_cities_dict[city].state_id == self.id:
-                cities_list.append(all_cities_dict[city])
-        return cities_list
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = Relationship('City',
+                            backref='states',
+                            cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            """Returns a list of cities that have the same state_id as State.id."""
+            from models import storage
+            all_cities_dict = storage.all(City)
+            cities_list = []
+            for city in all_cities_dict.keys():
+                if all_cities_dict[city].state_id == self.id:
+                    cities_list.append(all_cities_dict[city])
+            return cities_list
